edfbcedacc4b56a0efbadd26a704ba2e5d5f56c5
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2024-10-11 21:17:09 +0100

    first commit

---
+++ v04.js
@@ -0,0 +1,6 @@
+function clickOnMenu() {
+	document.getElementById("menu").classList.toggle("display-menu");
+	document.getElementById("top-bar").classList.toggle("toggle-top-bar");
+	document.getElementById("mid-bar").classList.toggle("toggle-mid-bar");
+	document.getElementById("btm-bar").classList.toggle("toggle-btm-bar");
+}
